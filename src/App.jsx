230d6a628ba80b9f98f053a13192d88dc0c17c1c
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-18 12:07:09 +0000

    enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom"
 
+
 function App() {
 
   return (
     <>
+    <Routes>
+
+      {/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
+      <Route path='/login' element={}/>  */}
+
+      {/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
+      <Route path='/register' element={}/> */}
+
+      {/* Falta importar el componente e insertar el componente HomePage dentro de las {}
+      <Route path='/home' element={}/> */}
+
+      {/* Falta importar el componente e insertar el componente DashboardPage dentro de las {}
+      <Route path='/dashboard' element={}/> */}
+
+      {/* Falta importar el componente e insertar el componente FavoritesPage dentro de las {}
+      <Route path='/favorites' element={}/> */}
+
+    </Routes>
       
     </>
   )
