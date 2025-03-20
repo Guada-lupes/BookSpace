59f84fa20e309828c24563cab31ed63997e770e6
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-20 09:32:30 +0000

    Estructura base del renderizado

--- src/components/ProductsComponent.jsx
+++ src/components/ProductsComponent.jsx
@@ -1,15 +1,25 @@
-import { useContext } from "react"
-import { BooksContext } from "../contexts/BooksContext"
+import { useContext } from "react";
+import { BooksContext } from "../contexts/BooksContext";
 
 export const ProductsComponent = () => {
+  //nos traemos la data de libros del contexto
+  const { books } = useContext(BooksContext);
 
-const {books} = useContext(BooksContext);
-
-console.log(books);
 
   return (
-    <div>{
-        books.map((libro)=>
-        (<p key={libro.id}>{libro.titulo}</p>))}</div>
-  )
-}
+    <div className="library-list">
+      {books.map((book) => (
+        <div key={book.id} className="library-item">
+          <div className="book-img-container"><img className="book-img" src={book.imagen} alt={`imagen libre relacionada con el nombre del libro: ${book.title}`} /></div>
+          
+          <div className="book-name-container"><p className="book-title">{book.titulo}</p> <p className="book-autor">{book.autor}</p></div>
+
+          <div className="book-genero-container"><p className="book-genero">{book.genero}</p></div>
+          <div className="book-rating-container"><p className="book-rating">{book.rating}</p></div>
+          
+          <div className="book-favoritos-container"></div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/contexts/BooksContext.jsx
+++ src/contexts/BooksContext.jsx
@@ -1,19 +1,19 @@
 import { createContext, useState, useEffect } from "react"
 import { getAllBooks } from "../services/booksService";
 
+//creamos el contexto
 export const BooksContext = createContext();
 
+//creamos el proovedor de contexto
 export const BooksProvider = ({children}) => {
-
+//esta useState guardará la data de libros
     const [books, setBooks] = useState([])
-
+//simulamos la peticion a la api y guardamos en books la data
 useEffect(()=>{
 
 setBooks(getAllBooks())
 
 }, [])
-
-console.log(books);
 
   return (
     <BooksContext.Provider value={{books}}>
