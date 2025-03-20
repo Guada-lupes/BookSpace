bdbb759005b99b62605ced6a06c12f8e1bde42c6
Sppgg <saracodestheweb@gmail.com>
2025-03-20 12:55:09 +0000

    Added DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -10,7 +10,27 @@ const DashboardPage = () => {
     return null;
   }
 
-  return <div>DashboardPage</div>;
+  return (
+    <div className="dashboard">
+      <h2 className="dashboard__title">
+        {" "}
+        ¡Bienvenido, {currentUser.fullName}!
+      </h2>
+      <div className="dashboard__user-info">
+        <img
+          src={currentUser.profilePicture}
+          alt={`Perfil de ${currentUser.userName}'`}
+          className="dashboard__profile-picture"
+        />
+      </div>
+      <div className="dashboard__user-details">
+        <p className="dashboard__detail">Sobre mí: {currentUser.about}</p>
+        <p className="dashboard__detail">Email: {currentUser.email}</p>
+        <p className="dashboard__detail">Edad: {currentUser.age}</p>
+        <p className="dashboard__detail">País: {currentUser.country}</p>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
