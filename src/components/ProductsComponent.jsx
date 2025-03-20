f76b057810da4963f6a5a2cddaa39a9d162e6b0e
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-20 17:39:25 +0000

    Creación de contexto y carrito para favoritos

--- src/components/ProductsComponent.jsx
+++ src/components/ProductsComponent.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { BooksContext } from "../contexts/BooksContext";
+import { FavoritesContext } from "../contexts/FavoritesContext";
 import { Link } from "react-router-dom";
 
 export const ProductsComponent = () => {
   //nos traemos la data de libros del contexto
   const { books } = useContext(BooksContext);
+  
+  const { favorites, addFavorites } = useContext(FavoritesContext)
 
   return (
     <div className="library-list">
@@ -34,8 +37,9 @@ export const ProductsComponent = () => {
             <button className="book-masinfo-buttton">Más info</button>
           </Link>
           <div className="book-favoritos-container">
+            
             {/* corazon vacio
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+          <svg onClick={()=>addFavorites(book.id)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
   <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
 </svg>
 corazon lleno
