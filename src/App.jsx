db3fb9ad2440e188674739966b1cb3b48fcfe526
Sppgg <saracodestheweb@gmail.com>
2025-03-21 20:43:40 +0000

    adding route path from App to DashboardPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,29 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 //Importación de componentes
-import { HomePage } from './pages/HomePage';
-import { FavoritesPage } from './pages/FavoritesPage';
-import { DetailsProductComponent } from './components/DetailsProductComponent';
+import { HomePage } from "./pages/HomePage";
+import { FavoritesPage } from "./pages/FavoritesPage";
+import { DetailsProductComponent } from "./components/DetailsProductComponent";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <>
-    <Routes>
-
-            {/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
+      <Routes>
+        {/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
 <Route path='/login' element={}/>  */}
 
-            {/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
+        {/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
 <Route path='/register' element={}/> */}
 
-            <Route path="/" element={<HomePage />}>
-              <Route path="/:id" element={<DetailsProductComponent />} />
-            </Route>
-
-            {/* Falta importar el componente e insertar el componente DashboardPage dentro de las {}
-<Route path='/dashboard' element={}/> */}
-
-<Route path='/favorites' element={<FavoritesPage/>}/>
-
-</Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="/:id" element={<DetailsProductComponent />} />
+        </Route>
+        <Route path="/dashboard" element={DashboardPage}>
+          {" "}
+        </Route>
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
     </>
   );
 }
