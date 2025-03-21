61ca03bff24de301b9f1a6adc7cff3185635aa0a
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-21 17:19:20 +0000

    merge feature/Rebe/Tarea1 y arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,52 @@
-import './App.css';
-{/*import { Routes, Route } from "react-router-dom"*/}
-
-
-import  HeaderComponent from "./components/HeaderComponent.jsx";
-import FooterComponent from "./components/FooterComponent.jsx"
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
+
 //Importación de contextos
-import { BooksProvider } from './contexts/BooksContext';
-import { FavoritesProvider } from './contexts/FavoritesContext';
+import { BooksProvider } from "./contexts/BooksContext";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
+
+//Importación de páginas
+import { HomePage } from "./pages/HomePage";
+
 //Importación de componentes
-import { HomePage } from './pages/HomePage';
-import { DetailsProductComponent } from './components/DetailsProductComponent';
+import { DetailsProductComponent } from "./components/DetailsProductComponent";
+import HeaderComponent from "./components/HeaderComponent.jsx";
+import FooterComponent from "./components/FooterComponent.jsx";
 
 function App() {
-
   return (
-    <div>
-      <HeaderComponent/>
-      
-      <FooterComponent/>
-    </div>
-  )
-   {/*<Routes>*/}
     <>
-    <FavoritesProvider>
-    <BooksProvider>
-    <Routes>
+      {/* Proveedor de contexto de Favoritos */}
+      <FavoritesProvider>
+        {/* Proveedor de contexto de los libros */}
+        <BooksProvider>
+          <Routes>
+            <HeaderComponent />
 
-{/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
+            {/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
 <Route path='/login' element={}/>  */}
 
-{/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
+            {/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
 <Route path='/register' element={}/> */}
 
+            <Route path="/" element={<HomePage />}>
+              <Route path="/:id" element={<DetailsProductComponent />} />
+            </Route>
 
-<Route path='/' element={<HomePage/>}>
-<Route path='/:id' element={<DetailsProductComponent/>}/>
-</Route>
-
-{/* Falta importar el componente e insertar el componente DashboardPage dentro de las {}
+            {/* Falta importar el componente e insertar el componente DashboardPage dentro de las {}
 <Route path='/dashboard' element={}/> */}
 
-{/* Falta importar el componente e insertar el componente FavoritesPage dentro de las {}
+            {/* Falta importar el componente e insertar el componente FavoritesPage dentro de las {}
 <Route path='/favorites' element={}/> */}
 
-    {/*</Routes>*/}
-  
-  
-</Routes>
-    </BooksProvider>
-    </FavoritesProvider>
+            {/*</Routes>*/}
+
+            <FooterComponent />
+          </Routes>
+        </BooksProvider>
+      </FavoritesProvider>
     </>
-  )
+  );
 }
 
 export default App;
-
