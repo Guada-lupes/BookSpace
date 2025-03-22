52c89632a2afb64bc644aa4f43165cad874e9405
Sppgg <saracodestheweb@gmail.com>
2025-03-22 19:53:57 +0000

    creating folder for homepage's style

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,40 +1,51 @@
 import { useState } from "react";
-import '../styles/HeaderStyle.css'; //Ruta de donde coge estilos el componente Header.
-
+import "../styles/HeaderStyle.css"; //Ruta de donde coge estilos el componente Header.
 
 const HeaderComponent = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
-    /*MENÚ HAMBURGUESA*/
+  /*MENÚ HAMBURGUESA*/
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
 
   return (
-    <header className="header" style={{ backgroundColor: '#d21b53' }}>
+    <header className="header" style={{ backgroundColor: "#d21b53" }}>
       <div className="header__logo-img">
-        <img src= "../src/assets/icons/logo.png" alt="Logo" style={{ marginLeft: '10px' }} />
+        <img
+          src="../src/assets/icons/logo.png"
+          alt="Logo"
+          style={{ marginLeft: "10px" }}
+        />
       </div>
-      <div className="header__title" style={{ fontFamily: 'Roboto', fontSize: '40px'}}>
+      <div
+        className="header__title"
+        style={{ fontFamily: "Roboto", fontSize: "40px" }}
+      >
         BOOKSPACE
       </div>
-      
+
       <div className="header__menu-icon" onClick={toggleMenu}>
-        <svg viewBox="0 0 100 80" width="40" height="40"> 
-          <rect width="100" height="20" fill="#333333"></rect> {/*fill ="color" rellena rect del color elegido"*/}
+        <svg viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="20" fill="#333333"></rect>{" "}
+          {/*fill ="color" rellena rect del color elegido"*/}
           <rect y="30" width="100" height="20" fill="#333333"></rect>
           <rect y="60" width="100" height="20" fill="#333333"></rect>
         </svg>
-      
       </div>
       {isMenuOpen && (
         <div className="header__menu-overlay">
           <ul className="header__menu-list">
-            <li><a href="Mi Perfil">Mi Perfil</a></li>
-            <li><a href="Mis Favoritos">Mis Favoritos</a></li>
-            <li><a href="Logout">Logout</a></li>
+            <li>
+              <a href="Mi Perfil">Mi Perfil</a>
+            </li>
+            <li>
+              <a href="Mis Favoritos">Mis Favoritos</a>
+            </li>
+            <li>
+              <a href="Logout">Logout</a>
+            </li>
           </ul>
         </div>
-
       )}
     </header>
   );
