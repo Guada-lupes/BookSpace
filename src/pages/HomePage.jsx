956c60d1e2b13bef99152063c2c11cd3a013b2b5
Sppgg <saracodestheweb@gmail.com>
2025-03-22 20:30:23 +0000

    Added HomePage's Style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,17 @@
-import { ProductsComponent } from "../components/ProductsComponent"
+import { ProductsComponent } from "../components/ProductsComponent";
 import { DetailsProductComponent } from "../components/DetailsProductComponent";
 import { useParams } from "react-router-dom";
+import "../styles/HomePageStyle.css";
 
 export const HomePage = () => {
-  const {id} = useParams();
+  const { id } = useParams();
 
   return (
-    <>
-    {
-      id ? (<DetailsProductComponent/>) :(<ProductsComponent/>)
-    }
-    
-    
-    </>
-
-  )
-}
+    <div className="home-page">
+      <h1 className="home-page__title">Tu comunidad literaria</h1>
+      <div className="home-page__content">
+        {id ? <DetailsProductComponent /> : <ProductsComponent />}
+      </div>
+    </div>
+  );
+};
