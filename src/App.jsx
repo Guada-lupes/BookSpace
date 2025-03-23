67adb1fdbcf2aed7e6cb57fc7637a8ae28456af8
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-23 21:45:40 +0000

    Se quitan cierrre de rutas simples

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,10 @@ function App() {
     <>
       <HeaderComponent />
       <Routes>
-        {/* Falta importar el componente e insertar el componente LoginPage dentro de las {}
-<Route path='/login' element={}/>  */}
-
-        {/* Falta importar el componente e insertar el componente RegisterPage dentro de las {
-<Route path='/register' element={}/> */}
-
         <Route path="/" element={<HomePage />}>
           <Route path="/:id" element={<DetailsProductComponent />} />
         </Route>
-        <Route path="/dashboard" element={<DashboardPage/>}>
-        </Route>
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
       <FooterComponent />
