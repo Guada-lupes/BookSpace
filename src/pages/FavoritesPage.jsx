46384675ce1a457c5f8d2c407047ebe8953957e6
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-24 18:30:37 +0000

    creación de BackButtom, añadido en FavoritePage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -3,12 +3,16 @@ import { EmptyCartComponent } from "../components/EmptyCartComponent";
 import React from 'react'
 import { useContext } from "react";
 import { FavoritesContext } from "../contexts/FavoritesContext";
+import { BackButton } from "../components/BackButton";
 
 export const FavoritesPage = () => {
 const { favorites }= useContext(FavoritesContext)
-  return (
+const url = "/home";
     
+  return (
+
     <div>
+      <BackButton url={url}/>
       {
         favorites.length === 0 ?  <EmptyCartComponent/> : <FavoritesCartComponent/>
       }
