b5dc7bee543a346a67607e2a9dab2fd341359bcc
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-25 16:30:24 +0000

    css básico de carrito fav

--- src/components/FavoritsCartComponent.jsx
+++ src/components/FavoritsCartComponent.jsx
@@ -1,39 +1,48 @@
 import { useContext } from "react";
 import { FavoritesContext } from "../contexts/FavoritesContext";
 import { Link } from "react-router-dom";
+import "../styles/FavoritesCartComponentStyle.css";
 
 export const FavoritesCartComponent = () => {
   const { deleteFavorites, favorites } = useContext(FavoritesContext);
 
-  console.log(favorites.map((favorite)=>favorite.titulo));
-  
+  console.log(favorites.map((favorite) => favorite.titulo));
+
   return (
     <div className="favorites-container">
-          <h1 className="favorites-container-title">Mis favoritos</h1>
+      <h1 className="favorites-container-title">Mis favoritos</h1>
       {favorites.map((favorite) => (
-        
         <>
           <div key={favorite.id} className="favorite-item">
-            <div className="favorite-item-info">
-            <h3 className="favorite-item-title">{favorite.titulo}</h3>
-            <p className="favorite-item-autor">{favorite.autor}</p>
-            </div>
-            <div className="favorite-item-img-container">
-            <img className="favorite-img" src={favorite.imagen} alt={`miniatura de la imagen relacionada con el titulo ${favorite.titulo}`} />
-            </div>
-         
-            {/* Cambiar esto por un icono de corazon acompañado de eliminar de favoritos*/}
-            <button onClick={() => deleteFavorites(favorite.id)}>Eliminar</button>
-            <Link to={`/${favorite.id}`}>
-            <button>Detalles</button>
-            </Link> 
-           
+
+          <div className="favorite-item-img-container">
+            <img
+              className="favorite-img"
+              src={favorite.imagen}
+              alt={`miniatura de la imagen relacionada con el titulo ${favorite.titulo}`}
+            />
           </div>
+            <div className="favotire-item-options">
+            <div className="favorite-item-info">
+              <h3 className="favorite-item-title">{favorite.titulo}</h3>
+              <p className="favorite-item-autor">{favorite.autor}</p>
+            </div>
 
+            {/* Cambiar esto por un icono de corazon acompañado de eliminar de favoritos*/}
+            <div className="favorite-item-buttons">
+            <Link to={`/${favorite.id}`}>
+              <button className="details-button">Detalles</button>
+            </Link>
+            <button className="quit-button" onClick={() => deleteFavorites(favorite.id)}>
+              Quitar
+            </button>
+            </div>
+            </div>
+
+
+          </div>
         </>
-
       ))}
-
     </div>
   );
 };
