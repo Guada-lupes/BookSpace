e6dad13acc26653d72b84216ebf412757f765974
Sppgg <saracodestheweb@gmail.com>
2025-03-29 14:15:56 +0000

    reestructuring component AdminPage.jsx and its CSS

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { BooksProvider } from "./contexts/BooksContext.jsx";
 import { FavoritesProvider } from "./contexts/FavoritesContext.jsx";
 import { AdminsBooksProvider } from "./contexts/AdminsBooksContext.jsx";
 import { UsersProvider } from "./contexts/UsersContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -13,22 +13,29 @@ const AdminPage = () => {
   }
 
   return (
-    <div className="admin-page">
-      <header className="admin-page__header">
-        <h2 className="admin-page__title">Panel de Administración</h2>
-        <nav className="admin-page__nav">
-          {/* Enlaces a distintas secciones */}
-          <Link className="admin-page__nav-link" to="users">
+    <div className="admin-container">
+      <aside className="admin-sidebar">
+        <div className="admin-sidebar__header">
+          <img
+            src={currentUser.profilePicture}
+            alt={currentUser.fullName}
+            className="admin-sidebar__avatar"
+          />
+          <h2 className="admin-sidebar__title">{currentUser.fullName}</h2>
+        </div>
+        <nav className="admin-sidebar__nav">
+          <Link className="admin-sidebar__link" to="users">
             Lista de Usuarios
           </Link>
-          <Link className="admin-page__nav-link" to="books">
+          <Link className="admin-sidebar__link" to="books">
             Gestión de Libros
           </Link>
         </nav>
-      </header>
-      <div className="admin-page__content">
-        <Outlet /> {/* Rutas hijas se renderizan aquí */}
-      </div>
+      </aside>
+      <main className="admin-content">
+        <Outlet />{" "}
+        {/* Aquí se renderizan las rutas hijadas: AdminUsersPage o AdminBooksPage */}
+      </main>
     </div>
   );
 };
