82e5e949dba7c157500285fb7b7e1e3d8c5657bd
Guada-lupes <gma.pedagoga@gmail.com>
2025-03-29 15:09:47 +0000

    añadido index.css en main nuevamente

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { BooksProvider } from "./contexts/BooksContext.jsx";
 import { FavoritesProvider } from "./contexts/FavoritesContext.jsx";
 import { AdminsBooksProvider } from "./contexts/AdminsBooksContext.jsx";
 import { UsersProvider } from "./contexts/UsersContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
