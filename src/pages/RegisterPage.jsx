38f7c117f961553b0687fa908e4406d54290715a
saramartin <sara1997mf@gmail.com>
2025-03-30 07:09:19 +0100

    validación enfuncionamiento

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const RegisterPage = () => {
+  const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [fullName, setFullName] = useState("");
@@ -10,9 +10,30 @@ const RegisterPage = () => {
   const { registerUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const [error, setError] = useState("");
+  const [emailError, setEmailError] = useState("");
 
+  // Función para validar el correo
+  const validateEmail = (value) => {
+    setEmail(value);
+    // Expresión regular para validar el formato del correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    // Si el correo no coincide con el formato, se muestra un error
+    if (!emailRegex.test(value)) {
+      setEmailError("Correo no válido");
+    } else {
+      setEmailError(""); //si el correo es válido no habrá error
+    }
+  };
+
+  //Función para manejar envío formulario
   const handleSubmit = (e) => {
     e.preventDefault();
+
+  //Si hay error no se enviará el formulario
+  if (emailError) {
+    return;
+  }
     
     const newUserData = { username, password, fullName, email };
     const result = registerUser(newUserData);
@@ -27,7 +48,7 @@ const RegisterPage = () => {
   return (
     <div>
       <h2>Registrar Usuario</h2>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -54,10 +75,12 @@ const RegisterPage = () => {
           type="email"
           placeholder="Correo electrónico"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => validateEmail(e.target.value)}
           required
         />
-        <button type="submit">Registrar</button>
+        {/* Mostramos el error de correo si es necesario */}
+        {emailError && <p className="error">{emailError}</p>}
+        <button type="submit" disabled={!!emailError}>Registrar</button>
       </form>
     </div>
   );
